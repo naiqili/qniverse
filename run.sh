f77abe5695cdf4460a0ece3307e955ba92dbed10
naiqili <linaiqi.thu@gmail.com>
2024-12-03 20:22:44 +0800

    1203

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 # python reporter/model/gdbt_pred.py --today '2024-12-02'
-python reporter/model/gdbt_fig.py --btstart '2024-03-01' --btend '2024-12-01'
+# python reporter/model/gdbt_fig.py --btstart '2024-03-01' --btend '2024-12-01'
+
+python reporter/model/olmar.py --btstart '2024-03-01' --btend '2024-12-01'
